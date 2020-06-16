58c884d7712dffc530142faa83056cdf0b835bc6
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-16 17:10:51 +0200

    version phoenix

--- views/Inscription.php
+++ views/Inscription.php
@@ -3,17 +3,17 @@
 <head>
     <meta charset="utf-8" />
     <title>Le Faux Twitter inscription</title>
-    
+
   <link rel="stylesheet" href="css/styleInscription.css">
 </head>
 
 <body>
 
 
-    <h1>Rejoignez notre communauté!!!</h1>
+    <h1>Rejoignez notre contrefacon de Twitter !!!</h1>
     <form method="post" action="/projet_bd/pages/traitement">
         <ul class="form-style-1">
-    
+
 		<!--Name field-->
 			<div class="form-group">
 				<label class="control-label" for="name">Nom:</label>
@@ -40,41 +40,42 @@
 				<input type="email" class="form-control" id="email" name="email"
 					required="required"/>
 			</div>
-			
+
 			<!--<?php if(isset($_SESSION['flashInscription'])) {
             echo "
            <p style='color: red'>
-            " . $_SESSION['flashInscription'] . " 
+            " . $_SESSION['flashInscription'] . "
            </p>
         ";
         }
         ?>-->
-			
+
 			<!--Password field-->
 			<div class="form-group">
 				<label class="control-label" for="password">Mot de passe: </label>
 				<input type="password" class="form-control" id="password" name="password"
 					required="required"/>
-			</div>	
+			</div>
 			<!--PasswordConfirmation field-->
 			<div class="form-group">
 				<label class="control-label" for="password_confirm">Confirmer votre mot de passe:</label>
 				<input type="password" class="form-control" id="password_confirm" name="password_confirm"
 					required="required"/>
-			</div>	
-			
+			</div>
+
 			<div>
 				<label for="birth">Votre date de naissance<br/></label>
 				<input type="date" name="birth" id="birth" class="field-divided" placeholder="birth"/>
 			</div>
-			
+
 			<input type="submit" class="btn btn-primary" value="Inscription" name="register"/
-			
+
 		</form>
         </ul>
     </form>
 
-<img src="./view/img/twitter_bird.png" HEIGHT="700">
+    <img class="phoenix1" src="img/Phoenix_right.png" HEIGHT="350" alt="un phoenix a droite de l'ecran">
+    <img class="phoenix2" src="img/Phoenix_left.png" HEIGHT="350" alt="un phoenix a gauche de l'ecran">
 <!-- Le pied de page -->
 
 <!--<?php include("pied_de_page.php"); ?>-->
