2a44cff04ddcf75d60263c249333a5981de05f70
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-17 21:05:48 +0200

    petite modif

--- views/profilLike.php
+++ views/profilLike.php
@@ -15,7 +15,11 @@
 <!-- Petit menu: tweets, abonnements, abonnés, j'aime-->
 
 <section>
-    <a href="profilTweet.php"><h4>Tweets</h4></a><a href="profilAbonnement.php"><h4>Abonnements</h4></a><a href="profilAbonnes.php"><h4>Abonnés</h4></a><a href="profilLike.php"><h4>J'aime</h4></a><a href="ChangeProfil.php"><h14>Editer le profil</h14></a>
+    <a href="profilTweet.php"><h4>Tweets</h4></a
+      ><a href="profilAbonnement.php"><h4>Abonnements</h4></a>
+      <a href="profilAbonnes.php"><h4>Abonnés</h4></a>
+      <a href="profilLike.php"><h4>J'aime</h4></a>
+      <a href="ChangeProfil.php"><h14>Editer le profil</h14></a>
 </section>
 
 <!-- courte description du profil-->
@@ -30,10 +34,11 @@
         <?php if ($tweets["tweet_id"] == $_SESSION['id']) {?> <a href="Le-Faux-Tweeter/traitementSuppTweet/<?php echo $tweets["tweet_id"] ?>  <h15>Supprimer</h15></a> <?php }?>
         <h8><?php echo $tweets["user_name"]; ?></h8><h10><?php echo $tweets["tweet_date"];?></h10>
         <h9><?php echo $tweets["tweet_content"]; ?></h9>
-        <a href="/Le-Faux-Tweeter/traitementRetweet/<?php echo $tweets["tweet_id"] ?>  <h10>Retweeter</h10> </a><a href="traitementLike.php/<?php echo $tweets["tweet_id"] ?><h12>J'aime </h12> </a>
+        <a href="/Le-Faux-Tweeter/traitementRetweet/<?php echo $tweets["tweet_id"] ?>  <h10>Retweeter</h10> </a>
+        <a href="traitementLike.php/<?php echo $tweets["tweet_id"] ?><h12>J'aime </h12> </a>
     </li>
 <?php endforeach; ?>
-    
+
 <img class="phoenix1" src="img/Phoenix_right.png" HEIGHT="350" alt="un phoenix a droite de l'ecran">
 <img class="phoenix2" src="img/Phoenix_left.png" HEIGHT="350" alt="un phoenix a gauche de l'ecran">
 <!-- Le pied de page -->
