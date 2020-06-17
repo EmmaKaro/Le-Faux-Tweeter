9ae950ad1bdcaf69805f36b9a82e527a04fb82fb
Nephera-63 <64312022+Nephera-63@users.noreply.github.com>
2020-06-17 17:54:34 +0200

    rajout phoenix

--- views/ChangeProfil.php
+++ views/ChangeProfil.php
@@ -31,6 +31,8 @@
     </form>
 </article>
 
+<img class="phoenix1" src="img/Phoenix_right.png" HEIGHT="350" alt="un phoenix a droite de l'ecran">
+<img class="phoenix2" src="img/Phoenix_left.png" HEIGHT="350" alt="un phoenix a gauche de l'ecran">
 <!-- Le pied de page -->
 
 <?php include("pied_de_page.php"); ?>
